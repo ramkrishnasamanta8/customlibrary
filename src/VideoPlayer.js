2e4a7ec6641ad72516c6395d5409fc4340bf025b
ramkrishnasamanta8 <ramkrishnasamanta8@gmail.com>
2024-11-28 13:26:28 +0530

    add web feature to this library

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-// import Video from 'react-native-video';
+ import Video from 'react-native-video';
 
 const VideoPlayer = ({ url,deviceType }) => {
   return (
